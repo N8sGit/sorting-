b690fa93c078e6d76039880919fd88da85a0d248
Jacob Kayen <jacob.kayen@gmail.com>
2017-03-02 15:10:07 -0500

    solved bubble sort

--- bubblesort.js
+++ bubblesort.js
@@ -1,21 +1,19 @@
-function bubbleSort(array){
-  if(array.length<=1) return array
-
-  for(var i = 0; i<array.length; i++){
-    var temp
-    for(var j = i+1 ; j<array.length; j++){
-      if(array[i]> array[j]){
-        temp = array[j];
-        array[j] = array[i]
-        array[i] = temp
-      }
-    }
-
-
+var swap = function(arr, i, j) {
+    if (arr[i] > arr[j]) {
+      var temp = arr[j];
+      arr[j] = arr[i];
+      arr[i] = temp; 
+  }
 }
 
-
-  console.log(array)
-
+function bubbleSort(array){
+  if (array.length <= 1) return array
+  for (var i = 0; i < array.length; i++){
+    for (var j = i + 1 ; j < array.length; j++){
+      swap(array, i, j);
+    }
+  }
   return array
 }
+
+
--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -1,24 +1,28 @@
 describe('Bubble Sort', function(){
+  beforeEach(function () {
+    spyOn(window, 'swap').and.callThrough();
+  });
+
   it('handles an empty array', function(){
     expect( bubbleSort([]) ).toEqual( [] );
   });
 
   it(' handles an array of one element', function(){
     expect(bubbleSort([1])).toEqual([1]);
+    console.log(swap.calls.count());
+    expect(swap.calls.count()).toEqual(0);
   });
 
   it('handles an array with an even number of elements', function(){
-
     expect(bubbleSort([2,8,9,8,3,7,8,6])).toEqual([2,3,6,7,8,8,8,9]);
-
+    console.log(swap.calls.count());
   });
-
 
   it('handles an array with an odd number of elements', function(){
-
     expect(bubbleSort([17,8,20,13,3,7,5,1,10,19,11])).toEqual([1,3,5,7,8,10,11,13,17,19,20]);
-
+    console.log(swap.calls.count());
   });
+
 
 
 
