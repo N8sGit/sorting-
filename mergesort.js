c1fe9454607ea1b40dd5968127c08fe0ec2889ef
Nathan Anecone <nanecone.gmail.com>
2017-03-02 15:51:04 -0500

    yadayada

---
+++ mergesort.js
@@ -0,0 +1,33 @@
+function split(wholeArray){
+  var half = Math.floor(wholeArray.length/2)
+  var firstHalf = wholeArray.slice(0, half)
+  var secondHalf = wholeArray.slice(half)
+  return [firstHalf,secondHalf]
+}
+
+
+function merge(one, two){
+  var result = [];
+  for(var i = 0; i<two.length; i++){
+    if(one[i]){
+      if(one[i]<two[i]){
+        result.push(one[i],two[i])
+      } else result.push(two[i],one[i])
+    }else result.push(two[i])
+
+  }
+  return result
+}
+
+
+function mergeSort(array){
+
+  if(array.length ===1) return array
+
+
+  else return split(array)
+
+
+
+
+}
